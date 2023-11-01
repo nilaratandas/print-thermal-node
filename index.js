5725fd46555979685d81818f28391ba2cc6267f4
nilardas <nilardas@cisco.com>
2023-11-01 18:20:43 +0530

    Modified files

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 const ThermalPrinter = require("node-thermal-printer").printer;
 const PrinterTypes = require("node-thermal-printer").types;
-const {PRINTER_CONFIG} = require("./constants")
+const { PRINTER_CONFIG } = require("./constants")
 
-let printer = new ThermalPrinter({
-  type: PrinterTypes.EPSON,
-  interface: `tcp://${PRINTER_CONFIG.IP}:${PRINTER_CONFIG.PORT}`
-});
+const electron = typeof process !== 'undefined' && process.versions && !!process.versions.electron;
+console.log('----electron----', electron);
+async function printSample() {
+  try {
+    let printer = new ThermalPrinter({
+      type: PrinterTypes.EPSON,
+      interface: `tcp://${PRINTER_CONFIG.IP}:${PRINTER_CONFIG.PORT}`
+    });
+    console.log(`Printer IP ${PRINTER_CONFIG.IP}: and Port is : ${PRINTER_CONFIG.PORT}`);
+    let printerStatus = await printer.isPrinterConnected();
 
-console.log(`Printer IP ${PRINTER_CONFIG.IP}: and Port is : ${PRINTER_CONFIG.PORT}`);
-
-const isPrinterConnected = async () => {
-    return await printer.isPrinterConnected();
-}
-
-
-const prnitProcess = async () => {
+    console.log("Printer Connection Status is :", printerStatus);
+    printer.alignCenter();
+    printer.drawLine();
+    printer.println("Hello world with TCP");
+    printer.drawLine();
+    console.log("------- Print Priview in Console -------\n", printer.getText());
+    printer.cut();
     try {
-        let execute = printer.execute()
-        console.log("Print done!");
-      } catch (error) {
-        console.error("Print failed:", error);
-      }
+      let execute = printer.execute()
+      console.log("Print done!");
+    } catch (error) {
+      console.error("Print failed:", error);
+    }
+  } catch (error) {
+    console.error("Print failed:", error);
+  }
 }
 
-
-
-isPrinterConnected().then(function (printerStatus){
-    console.log("Printer Connection Status is :", printerStatus );
-    if(printerStatus){
-        printer.alignCenter();
-        printer.println("Hello world with TCP");
-        printer.cut();
-        prnitProcess();
-    }
-});
-
-
-
-
+printSample();
--- print-with-system-driver.js
+++ print-with-system-driver.js
@@ -1,20 +1,35 @@
-const ThermalPrinter = require("node-thermal-printer").printer;
-const PrinterTypes = require("node-thermal-printer").types;
+const { ThermalPrinter, PrinterTypes, CharacterSet, BreakLine } = require("node-thermal-printer")
 const electron = typeof process !== 'undefined' && process.versions && !!process.versions.electron;
+const { PRINTER_CONFIG } = require("./constants")
 
-let printer = new ThermalPrinter({
-  type: PrinterTypes.EPSON,
-  interface: 'printer:My Printer',
-  driver: require(electron ? 'electron-printer' : 'printer')
-});
+console.log(electron);
 
-printer.alignCenter();
-printer.println("Hello world with TCP");
-printer.cut();
+async function printSample() {
+  try {
+    let printer = new ThermalPrinter({
+      type: PrinterTypes.EPSON,
+      interface: 'printer:My Printer',
+      driver: require(electron ? 'electron-printer' : 'printer')
+    });
+    console.log(`Printer IP ${PRINTER_CONFIG.IP}: and Port is : ${PRINTER_CONFIG.PORT}`);
+    let printerStatus = await printer.isPrinterConnected();
 
-try {
-  let execute = printer.execute()
-  console.log("Print done!");
-} catch (error) {
-  console.error("Print failed:", error);
+    console.log("Printer Connection Status is :", printerStatus);
+    printer.alignCenter();
+    printer.drawLine();
+    printer.println("Hello world with TCP");
+    printer.drawLine();
+    console.log("------- Print Priview in Console -------\n", printer.getText());
+    printer.cut();
+    try {
+      let execute = printer.execute()
+      console.log("Print done!");
+    } catch (error) {
+      console.error("Print failed:", error);
+    }
+  } catch (error) {
+    console.error("Print Error Handled:", error);
+  }
 }
+
+printSample();
